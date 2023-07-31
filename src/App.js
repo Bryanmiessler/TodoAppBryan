0ac48b394132361ebd0bc5f4e9a25370848d3c9a
Bryan <bryanmiessler@gmail.com>
2023-07-31 11:51:35 -0500

    iniciando repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './platzi.webp';
-import './App.css';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { CreateTodoButton } from './CreateTodoButton';
+import { TodoItem } from './TodoItem';
+
+const defaultTodos = [
+  { text: 'Armar escritorio', completed: true},
+  { text: 'Saltar Lazo', completed: true},
+  { text: 'Mercar', completed: false},
+  { text: 'Barrer', completed: false},
+  { text: 'Trapear', completed: true},
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoCounter completed={16} total={20} />
+      <TodoSearch />
+
+      <TodoList>
+        {defaultTodos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
+        ))}
+      </TodoList>
+
+      <CreateTodoButton />
+    </>
   );
 }
 
